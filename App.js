c63ab85eb83ef1668d7c2111635f0191b67def6f
ohnas <ohnas12@gmail.com>
2023-06-07 16:49:19 +0900

    Realm SDK

--- App.js
+++ App.js
@@ -1,8 +1,53 @@
+import Realm from "realm";
 import { NavigationContainer } from "@react-navigation/native";
-import React from "react";
+import * as SplashScreen from 'expo-splash-screen';
+import React, { useCallback, useEffect, useState } from "react";
 import Navigator from "./navigator";
 
+const FeelingSchema = {
+  name: "Feeling",
+  properties: {
+    _id: "int",
+    emotion: "string",
+    message: "string",
+  },
+  primaryKey: "_id",
+};
+
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+  async function prepare() {
+    try {
+      const realm = await Realm.open({
+        path: "diaryDB",
+        schema: [FeelingSchema],
+      });
+      console.log("is DB Open?", realm);
+    } catch (e) {
+      console.warn(e);
+    } finally {
+      // Tell the application to render
+      setAppIsReady(true);
+    }
+  };
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+  useEffect(() => {
+    prepare();
+  }, [])
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
       <Navigator />
